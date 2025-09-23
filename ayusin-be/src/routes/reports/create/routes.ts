1d1ad09e46cc75ceefc5a89617fb5faeeb8b192b
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-23 17:22:49 +0800

    feat(reports): create report route success response schema

--- ayusin-be/src/routes/reports/create/routes.ts
+++ ayusin-be/src/routes/reports/create/routes.ts
@@ -3,18 +3,36 @@ import * as HttpStatusCodes from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
 import { z } from "zod";
 
-const tags = ["Reports"];
+// TODO: Use proper float32/64 depending on db
+const Location = z.object({
+  x: z.number(),
+  y: z.number(),
+});
 
-const what = z.object({
-  message: z.string(),
+const SuccessResponseSchema = z.object({
+  status: z.literal("success"),
+  id: z.string(),
+  title: z.string(),
+  description: z.string(),
+  // TODO: Ideally this is an enum.
+  category: z.string(),
+  labels: z.array(z.string()),
+  media_links: z.array(z.string()),
+  // TODO: Handle defaults db-wise
+  upvotes: z.number().min(0),
+  location: Location,
 });
 
 export const create = createRoute({
+  description: "Create a new report",
   path: "/reports",
   method: "post",
-  tags,
+  tags: ["Reports"],
   responses: {
-    [HttpStatusCodes.OK]: jsonContent(what, "Message"),
+    [HttpStatusCodes.OK]: jsonContent(
+      SuccessResponseSchema,
+      "Successfully created a new report",
+    ),
   },
 });
 
