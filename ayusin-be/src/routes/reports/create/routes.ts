771eef18231d8ae25fc00883dbdd10ca8a293ada
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-23 17:17:34 +0800

    chore: extract create handlers and routes to @/routes/reports/create/

--- ayusin-be/src/routes/reports/create/routes.ts
+++ ayusin-be/src/routes/reports/create/routes.ts
@@ -6,19 +6,16 @@ import { z } from "zod";
 const tags = ["Reports"];
 
 const what = z.object({
-    "message": z.string()
+  message: z.string(),
 });
 
 export const create = createRoute({
-    path: "/reports",
-    method: "post",
-    tags,
-    responses: {
-        [HttpStatusCodes.OK]: jsonContent(
-            what,
-            "Message"
-        )
-    }
-})
+  path: "/reports",
+  method: "post",
+  tags,
+  responses: {
+    [HttpStatusCodes.OK]: jsonContent(what, "Message"),
+  },
+});
 
 export type CreateRoute = typeof create;
--- ayusin-be/src/routes/reports/index.ts
+++ ayusin-be/src/routes/reports/index.ts
@@ -1,8 +1,7 @@
 import { createClerkRouter, createRouter } from "@/lib/create-app";
-import * as handlers from "./handlers";
-import * as routes from "./routes";
+import * as handlers from "./create/handlers";
+import * as routes from "./create/routes";
 
-const router = createClerkRouter()
-    .openapi(routes.create, handlers.create);
+const router = createRouter().openapi(routes.create, handlers.create);
 
 export default router;
