78e2b7d1b8c8a2aedafcc10aa73c5ab063483eb4
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-23 22:01:46 +0800

    feat(reports): get all reports route 200 401 404 responses

--- ayusin-be/src/routes/reports/index.ts
+++ ayusin-be/src/routes/reports/index.ts
@@ -1,9 +1,12 @@
 import { createClerkRouter, createRouter } from "@/lib/create-app";
 import { createReportRoute } from "./create/routes";
 import { createReportHandler } from "./create/handlers";
+import { getAllReportsRoute } from "./getAll/routes";
+import { getAllReportsHandler } from "./getAll/handlers";
 
 const router = createRouter()
 	.basePath("/reports")
-	.openapi(createReportRoute, createReportHandler);
+	.openapi(createReportRoute, createReportHandler)
+	.openapi(getAllReportsRoute, getAllReportsHandler);
 
 export default router;
