0433dbc65e15d5652fb9c20db7c6830959626feb
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-23 14:56:39 +0800

    nix: add initial flake and devShell

---
+++ ayusin-be/nix/devShell.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  extras,
+}:
+with pkgs;
+mkShell {
+  name = "ayusin-web/ayusin-be";
+
+  nativeBuildInputs = [
+    extras.pkgs-unstable.bun # ^1.2.22, may change based on nixpkgs/unstable
+    vtsls
+    biome
+  ];
+
+}
