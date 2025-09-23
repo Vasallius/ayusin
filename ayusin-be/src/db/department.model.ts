c8843fc9542ce19a33782ce9cd79f1198812bac5
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-23 13:05:18 +0800

    feat: add departments schema

---
+++ ayusin-be/src/db/department.model.ts
@@ -0,0 +1,24 @@
+import * as mongoose from "mongoose";
+import { pointSchema } from "./point.model";
+
+export const departmentSchema = new mongoose.Schema(
+	{
+		version: { type: String, required: true },
+		name: { type: String, required: true },
+		shortName: { type: String, required: false },
+		contact: { type: String, required: false },
+		email: { type: String, required: false },
+		headquarterAddress: { type: String, required: false },
+		headquarterLocation: { type: pointSchema, required: false },
+		members: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+	},
+	{ timestamps: true },
+);
+
+export type Department = mongoose.InferSchemaType<typeof departmentSchema>;
+export const Department = mongoose.model("Department", departmentSchema);
