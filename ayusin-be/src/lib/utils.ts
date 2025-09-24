3c2cfeccf022684c1f0cc1fd988373480605ed88
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-24 17:35:57 +0800

    feat(lib/utils): isNullOrUndefined function

--- ayusin-be/src/lib/utils.ts
+++ ayusin-be/src/lib/utils.ts
@@ -1,1 +1,7 @@
 export type ExtractBaseType<T, U> = Pick<T, keyof U>;
+
+export function isNullOrUndefined<T>(
+	value: T | null | undefined,
+): value is null | undefined {
+	return value === null || typeof value === "undefined";
+}
