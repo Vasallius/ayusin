f12351f1ccd380fcfea48e2b756111f2e64fbdf5
Angelo Fallaria <ba.fallaria@gmail.com>
2025-09-24 18:28:35 +0800

    feat: add schema for `roles` collection

---
+++ ayusin-be/src/db/role.model.ts
@@ -0,0 +1,20 @@
+import * as mongoose from "mongoose";
+
+export const roleSchema = new mongoose.Schema(
+	{
+		version: { type: Number, required: true },
+		name: { type: String, required: true },
+		description: { type: String, required: false },
+		parentRole: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: false,
+			ref: "Role",
+		},
+		clearanceLevel: { type: String, required: true },
+		accessScope: { type: [String], required: true },
+	},
+	{ timestamps: true },
+);
+
+export type Role = mongoose.InferSchemaType<typeof roleSchema>;
+export const Role = mongoose.model("Role", roleSchema);
