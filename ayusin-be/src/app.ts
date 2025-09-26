82f70a4c020919a7731be08d0cf1e563a3f91473
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-09-26 14:58:11 +0800

    feat(app): add onError handler that returns response 500 and logs using pino

--- ayusin-be/src/app.ts
+++ ayusin-be/src/app.ts
@@ -2,17 +2,26 @@ import createApp from "./lib/create-app";
 import configureOpenAPI from "./lib/openapi-spec";
 import index from "./routes/index";
 import reports from "./routes/reports/index";
+import * as HttpStatusCodes from "stoker/http-status-codes";
 
 const app = createApp();
 configureOpenAPI(app);
 
-const routes = [
-    index,
-    reports
-]
+app.onError(function (err, c) {
+	c.var.logger.error("Something went wrong!", err.cause, err.stack);
+	return c.json(
+		{
+			status: "error",
+			description: `Something went wrong: ${err.message}`,
+		},
+		HttpStatusCodes.INTERNAL_SERVER_ERROR,
+	);
+});
+
+const routes = [index, reports];
 
 routes.forEach((route) => {
-    app.route("/", route)
-})
+	app.route("/", route);
+});
 
 export default app;
