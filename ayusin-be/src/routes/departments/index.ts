1122e91042d30453347f456687c4c629e1abe5ab
Sheape <paulpare@protonmail.com>
2025-09-27 16:25:17 +0800

    feat: add update department properties by id

--- ayusin-be/src/routes/departments/index.ts
+++ ayusin-be/src/routes/departments/index.ts
@@ -1,10 +1,12 @@
 import { createClerkRouter } from "@/lib/create-app";
-import { create,createDepartment } from "./create/index";
+import { create, createDepartment } from "./create/index";
 import { getByID, getDepartmentByID } from "./getByID/index";
+import { updateByID, updateDepartmentByID } from "./updateByID/index";
 
 const routes = createClerkRouter()
     .basePath("/departments")
     .openapi(create, createDepartment)
-    .openapi(getByID, getDepartmentByID);
+    .openapi(getByID, getDepartmentByID)
+    .openapi(updateByID, updateDepartmentByID);
 
 export default routes
